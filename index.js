ee97511377032c6bcc48840a3bfb5f96c7bb9f5a
aspencass <caspenw1@gmail.com>
2022-08-08 17:53:04 -0600

    commit tree 5e8717319bae7bfcec66ce3609e1916aa65b8993

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // code your solution here
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
+
+
+/*
+
+
+function wrapAdjective() {
+const wrapAdjective = 'You are ${style}${adjective}${style}!'
+let emphatic = 'a hard worker'
+    if (style='*' && adjective='emphatic') {
+        return 'You are *a hard worker*!'
+    } else {
+        return 'You are |a dedicated programmer|!'
+    }
+}
+
+
+
+function wrapAdjective(style='*') {
+    let emphatic = 'a hard worker'
+    return  `You are ${style}${emphatic}${style}!`
+}
+
+
+*/
